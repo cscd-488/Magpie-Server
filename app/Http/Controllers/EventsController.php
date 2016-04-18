ada2aa0dc50772059debe0548cfc0c47347c7365
Josh Harshmamn <joshgreyhat@gmail.com>
2016-04-17 20:55:28 -0700

    pull-events-only
    add new functionality in EventsController

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -3,15 +3,59 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\Checkpoint;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventsController
 {
 
-    public function getEvents(Request $request)
+    public function pullData(Request $request) {
+
+
+        //evaluate request
+        $api_code = $request->header('requestcode');
+
+        if(is_numeric($api_code) || $api_code == null) {
+
+            switch($api_code) {
+                case 1:
+                    $this->getEventsWithLocations();
+                    break;
+                case 2:
+                    $this->getEvents();
+                    break;
+                case 3:
+                    $this->getLocationsByEvent($request);
+                default:
+                    abort('400', 'api code does not exist');
+            }
+
+        }
+        else {
+            abort('400', 'api code null or non numeric');
+        }
+
+    }
+
+    /* Return event's with locations */
+    private function getEventsWithLocations()
     {
         return Event::with('checkpoints')->get();
+    }
+
+    /* Return list of events excluding locations */
+    private function getEvents()
+    {
+        return Event::all();
+    }
+
+
+    // TODO:
+    /* Return list of locations by event */
+    private function getLocationsByEvent(Request $request) {
+        $checkpoint = new Checkpoint($request->header('data'));
+        return $checkpoint->with('event')->get();
     }
 
     public function postEvent(Request $request)
--- app/Models/Checkpoint.php
+++ app/Models/Checkpoint.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Checkpoint extends Model
 {
+    public function event() {
+        return $this->hasOne('event_id');
+    }
 
 }
