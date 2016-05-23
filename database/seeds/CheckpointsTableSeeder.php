4c37d9a3a3676c2dcdcfaeb84a7eb4733b16d299
Josh Harshmamn <joshgreyhat@gmail.com>
2016-05-22 17:01:28 -0700

    database-seeding
    seed integer and float types to database

--- database/seeds/CheckpointsTableSeeder.php
+++ database/seeds/CheckpointsTableSeeder.php
@@ -15,19 +15,19 @@ class CheckpointsTableSeeder extends Seeder
         DB::table('checkpoints')->insert(array(
 
             array('event_id'=>1, 'title'=>str_random(10), 'artist'=>str_random(10), 'description'=>str_random(10),
-                'image_src'=>'http://i.imgur.com/bsABe5c.jpg', 'lat'=>'47.49186', 'lon'=>'-117.581902', 'qr'=>str_random(5), 'status'=>0),
+                'image_src'=>'http://i.imgur.com/bsABe5c.jpg', 'lat'=>47.49186, 'lon'=>-117.581902, 'qr'=>str_random(5), 'status'=>0),
 
             array('event_id'=>2, 'title'=>str_random(10), 'artist'=>str_random(10), 'description'=>str_random(10),
-                'image_src'=>'http://i.imgur.com/bsABe5c.jpg', 'lat'=>'47.489631', 'lon'=>'-117.585299', 'qr'=>str_random(5), 'status'=>0),
+                'image_src'=>'http://i.imgur.com/bsABe5c.jpg', 'lat'=>47.489631, 'lon'=>-117.585299, 'qr'=>str_random(5), 'status'=>0),
 
             array('event_id'=>3, 'title'=>str_random(10), 'artist'=>str_random(10), 'description'=>str_random(10),
-                'image_src'=>'http://i.imgur.com/bsABe5c.jpg', 'lat'=>'47.492384', 'lon'=>'-117.584431', 'qr'=>str_random(5), 'status'=>0),
+                'image_src'=>'http://i.imgur.com/bsABe5c.jpg', 'lat'=>47.492384, 'lon'=>-117.584431, 'qr'=>str_random(5), 'status'=>0),
 
             array('event_id'=>4, 'title'=>str_random(10), 'artist'=>str_random(10), 'description'=>str_random(10),
-                'image_src'=>'http://i.imgur.com/bsABe5c.jpg', 'lat'=>'47.492442', 'lon'=>'-117.581289', 'qr'=>str_random(5), 'status'=>0),
+                'image_src'=>'http://i.imgur.com/bsABe5c.jpg', 'lat'=>47.492442, 'lon'=>-117.581289, 'qr'=>str_random(5), 'status'=>0),
 
             array('event_id'=>5, 'title'=>str_random(10), 'artist'=>str_random(10), 'description'=>str_random(10),
-                'image_src'=>'http://i.imgur.com/bsABe5c.jpg', 'lat'=>'47.507068', 'lon'=>'-117.566846', 'qr'=>str_random(5), 'status'=>0),
+                'image_src'=>'http://i.imgur.com/bsABe5c.jpg', 'lat'=>47.507068, 'lon'=>-117.566846, 'qr'=>str_random(5), 'status'=>0),
 
 
         ));
--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -15,23 +15,23 @@ class EventsTableSeeder extends Seeder
 
            array('title'=>str_random(10), 'short_title'=>str_random(10), 'author'=>str_random(10),
                'description'=>str_random(10), 'image_src'=>'http://i.imgur.com/bsABe5c.jpg',
-               'lat'=>'47.490519', 'lon'=>'-117.583796', 'qr'=>str_random(5)),
+               'lat'=>47.490519, 'lon'=>-117.583796, 'qr'=>str_random(5)),
 
            array('title'=>str_random(10), 'short_title'=>str_random(10), 'author'=>str_random(10),
                'description'=>str_random(10), 'image_src'=>'http://i.imgur.com/bsABe5c.jpg',
-               'lat'=>'47.489631', 'lon'=>'-117.585299', 'qr'=>str_random(5)),
+               'lat'=>47.489631, 'lon'=>-117.585299, 'qr'=>str_random(5)),
 
            array('title'=>str_random(10), 'short_title'=>str_random(10), 'author'=>str_random(10),
                'description'=>str_random(10), 'image_src'=>'http://i.imgur.com/bsABe5c.jpg',
-               'lat'=>'47.492224', 'lon'=>'-117.583201', 'qr'=>str_random(5)),
+               'lat'=>47.492224, 'lon'=>-117.583201, 'qr'=>str_random(5)),
 
            array('title'=>str_random(10), 'short_title'=>str_random(10), 'author'=>str_random(10),
                'description'=>str_random(10), 'image_src'=>'http://i.imgur.com/bsABe5c.jpg',
-               'lat'=>'47.49186', 'lon'=>'-117.581902', 'qr'=>str_random(5)),
+               'lat'=>47.49186, 'lon'=>-117.581902, 'qr'=>str_random(5)),
 
            array('title'=>str_random(10), 'short_title'=>str_random(10), 'author'=>str_random(10),
                'description'=>str_random(10), 'image_src'=>'http://i.imgur.com/bsABe5c.jpg',
-               'lat'=>'47.507375', 'lon'=>'-117.568042', 'qr'=>str_random(5)),
+               'lat'=>47.507375, 'lon'=>-117.568042, 'qr'=>str_random(5)),
 
         ));
     }
